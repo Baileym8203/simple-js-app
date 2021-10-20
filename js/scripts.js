92bea95b5465f80282a8700d2c9cc8bcaaa17c4e
bailey P Mashore <90733291+Baileym8203@users.noreply.github.com>
2021-10-20 13:26:25 -0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+let myName = 'bob'
+document.write(myName);
+myName = 'John Doe'
+document.write(myName);
+// Created varibles for John and bob
+/* Learning JS is fun!!
+  Love It!!*/
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
+let minSize = (doubleSize * 2) - (size/ 2);
+document.write(minSize);
