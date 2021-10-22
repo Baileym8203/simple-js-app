e88cac4a795c557dda9d72c2ceb157e4b24f6873
bailey P Mashore <90733291+Baileym8203@users.noreply.github.com>
2021-10-22 12:15:55 -0700

    practice + spacing fix
    if applied, this will remove my practice code as well as fix the spacing to make easier readable code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
-let pokemonList = [{name: 'bulbassour', height: 12, type: 'nature'},{name: 'charmander', height: 10, type:'fire'},
-    {name:'eve', height: 17, type: ['shock', 'water','fire']}];
-    console.log(pokemonList);
-    console.log(pokemonList[0].name);
-    console.log(pokemonList[1].name);
-    console.log(pokemonList[2].name);
-    console.log(pokemonList[0].type);
-    console.log(pokemonList[1].type);
-    console.log(pokemonList[2].type);
-    
+let pokemonList = [
+{name: 'bulbassour', height: 12, type: 'nature'},
+{name: 'charmander', height: 10, type:'fire'},
+{name:'eve', height: 17, type: ['shock', 'water','fire']}];
+
+console.log(pokemonList);
+console.log(pokemonList[0].name);
+console.log(pokemonList[1].name);
+console.log(pokemonList[2].name);
+console.log(pokemonList[0].type);
+console.log(pokemonList[1].type);
+console.log(pokemonList[2].type);
