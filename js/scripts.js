bdfd1cb60d4bac2183894aaf42fa8a75f9b14bbc
bailey P Mashore <90733291+Baileym8203@users.noreply.github.com>
2021-10-21 20:08:33 -0700

    semicolon missing
    if applied, this will add the missing semicolon after the pokemonList varible

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList = [{name: 'bulbassour', height: 12, type: 'nature'},{name: 'charmander', height: 10, type:'fire'},
-    {name:'eve', height: 17, type: ['shock', 'water','fire']}]
+    {name:'eve', height: 17, type: ['shock', 'water','fire']}];
     console.log(pokemonList);
     console.log(pokemonList[0].name);
     console.log(pokemonList[1].name);
