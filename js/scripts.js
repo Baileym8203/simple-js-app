0f1969f06834b7de7e0892e1eb8dd0afdf886be1
bailey P Mashore <90733291+Baileym8203@users.noreply.github.com>
2021-10-29 00:42:04 -0700

    quick fixes
    if applied, this will nest the if, else statements inside the  IIFE for local variables to be un-seeable

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,3 @@
-let pokemonList = [
-{name: 'bulbassour', height: 12, type: 'nature'},
-{name: 'charmander', height: 10, type:'fire'},
-{name:'eve', height: 30, type: ['shock', 'water','fire']}];
-
-pokemonList.forEach (function(pokemon){
-if (pokemon.height >= 15 && pokemon.height <= 17){
-  console.log(pokemon.name + ' is a juvinille!' );
-} else if (pokemon.height > 20 && pokemon.height > 25 || pokemon.height > 28){
-console.log(pokemon.name + ' is fully evolved!')
-} else {
- console.log(pokemon.name + ' is a baby!');
-};
- pokemonList.forEach(function(pokemon){
-  document.write('<ul>');
-  document.write(pokemon.name + ' - Height: ' + pokemon.height +'m');
-  if (pokemon.height > 12) document.write("   Wow, thats tall!");
-  document.write("</ul>");
- }
-)});
-
 let pokemonRepository = (function () {
 let pokemonList = [{name: 'bulbassour', height: 12, type: 'nature'},
 {name: 'charmander', height: 10, type:'fire'},
@@ -28,9 +7,7 @@ function add(pokemon) {
   pokemonList.push(pokemon);
 }
   
-
-
-  function getAll() {
+ function getAll() {
   return pokemonList;
  }
 
@@ -43,3 +20,20 @@ getAll: getAll
 console.log(pokemonRepository.getAll());
 pokemonRepository.add({name: 'pikachu', height: 7, type: 'shock'});
 console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function(pokemon){
+  if (pokemon.height >= 15 && pokemon.height <= 17){
+    console.log(pokemon.name + ' is a juvinille!' );
+  } else if (pokemon.height > 20 && pokemon.height > 25 || pokemon.height > 28){
+  console.log(pokemon.name + ' is fully evolved!')
+  } else {
+   console.log(pokemon.name + ' is a baby!');
+  };
+});
+
+pokemonRepository.getAll().forEach(function(pokemon){
+  document.write('<ul>');
+  document.write(pokemon.name + ' - Height: ' + pokemon.height +'m');
+  if (pokemon.height > 12) document.write("   Wow, thats tall!");
+  document.write("</ul>");
+ });
